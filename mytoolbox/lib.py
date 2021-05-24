e71c0eb09b01e43243e4ed52f517829d42d68614
Phil Lin <toogus@gmail.com>
2021-05-24 13:39:55 +0800

    Initial Push

---
+++ mytoolbox/lib.py
@@ -0,0 +1,5 @@
+def get_avg(*args):
+   summation = sum(args)
+   average = summation / len(args)
+   return average
+
---
+++ tests/test_avg.py
@@ -0,0 +1,4 @@
+from mytoolbox.lib import get_avg
+def test_avg():
+  assert get_avg(6,7,8) == 7
+
